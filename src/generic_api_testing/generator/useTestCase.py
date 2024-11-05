1915aaf5f7f5ec2596859a3706ea053bd4873d10
Luiz Gustavo Pina de Sales <luizgustavo.sales@opus-software.com.br>
2024-11-04 23:19:27 -0300

    Adjusting example name

--- src/generic_api_testing/generator/useTestCase.py
+++ src/generic_api_testing/generator/useTestCase.py
@@ -5,5 +5,5 @@ Test File
 from TestCase import TestCase
 from GenerateFeature import GenerateFeature
 
-generate_feature =GenerateFeature("generic_api_testing/generator/1.0.2.yml")
-generate_feature.create_feature("/business-accounts")
+generate_feature =GenerateFeature("generic_api_testing/generator/example.yml")
+generate_feature.create_feature("/example")
