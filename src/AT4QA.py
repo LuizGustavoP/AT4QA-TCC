7e993b70d77b107259c37dfc4c2f1ec0c4bf7876
Luiz Gustavo Pina de Sales <luizgustavo.sales@opus-software.com.br>
2024-12-09 22:55:06 -0300

    Establishing basic routes for saving files, uploading, listing, editor, generator and test builder

--- src/AT4QA.py
+++ src/AT4QA.py
@@ -1,5 +1,5 @@
 import subprocess
-from flask import Flask, Response, render_template, request, url_for, jsonify, send_from_directory
+from flask import Flask, Response, render_template, request, url_for, jsonify, send_from_directory, flash, redirect
 import os
 
 app = Flask(__name__)
@@ -17,54 +17,57 @@ app.config['DOCUMENTATION_UPLOAD_FOLDER'] = DOCUMENTATION_UPLOAD_FOLDER
 app.config["ALLOWED_FILE_EXTENSIONS"] = ["feature", "mask", "dictionary", "yaml", "json"]
 
 if not os.path.exists(app.config["EDITOR_UPLOAD_FOLDER"]):
-  os.makedirs(app.config["EDITOR_UPLOAD_FOLDER"])
+    os.makedirs(app.config["EDITOR_UPLOAD_FOLDER"])
 
 if not os.path.exists(app.config["FEATURE_UPLOAD_FOLDER"]):
-  os.makedirs(app.config["FEATURE_UPLOAD_FOLDER"])
+    os.makedirs(app.config["FEATURE_UPLOAD_FOLDER"])
 
 if not os.path.exists(app.config["DOCUMENTATION_UPLOAD_FOLDER"]):
-  os.makedirs(app.config["DOCUMENTATION_UPLOAD_FOLDER"])
+    os.makedirs(app.config["DOCUMENTATION_UPLOAD_FOLDER"])
 
 def check_file_extension(file):
-  return "." in file and file.rsplit(".", 1)[1].lower() in app.config["ALLOWED_FILE_EXTENSIONS"]
+    return "." in file and file.rsplit(".", 1)[1].lower() in app.config["ALLOWED_FILE_EXTENSIONS"]
 
 @app.route('/')
 def homepage():
-  return render_template(
-    'home.html',
-    title="Home - AT4QA Project"
-  )
+    return render_template(
+        'home.html',
+        title="Home - AT4QA Project"
+    )
 
 @app.route('/agbdd')
 def agbdd():
-  return render_template(
-    'agbdd.html',
-    title="AGBDD - AT4QA Project"
-  )
+    return render_template(
+        'agbdd.html',
+        title="AGBDD - AT4QA Project"
+    )
 
 @app.route('/generator')
 def generator():
-  return render_template(
-    'generator.html',
-    title="Generator - AT4QA Project"
-  )
+    return render_template(
+        'generator.html',
+        title="Generator - AT4QA Project"
+    )
 
 @app.route('/editor')
 def editor():
-  return render_template(
-    'editor.html',
-    title="Editor - AT4QA Project"
-  )
+    return render_template(
+        'editor.html',
+        title="Editor - AT4QA Project"
+    )
 
 @app.route('/test_builder')
 def test_builder():
-  return render_template(
-    'test_builder.html',
-    title="Builder - AT4QA Project"
-  )
+    return render_template(
+        'test_builder.html',
+        title="Builder - AT4QA Project"
+    )
 
-@app.route('/upload_test', methods=['POST'])
-def upload_test():
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+
+    type = request.args.get('type', '')
+
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
@@ -74,27 +77,42 @@ def upload_test():
         if file.filename == '':
             return jsonify({'error': 'No selected file'}), 400
 
-        if file:
+        if file and type == 'feature':
             file.save(os.path.join(app.config['FEATURE_UPLOAD_FOLDER'], file.filename))
+        elif file and type == 'documentation':
+            file.save(os.path.join(app.config['DOCUMENTATION_UPLOAD_FOLDER'], file.filename))
 
     return jsonify({'message': 'Files successfully uploaded'}), 200
 
 @app.route('/list_uploaded_files', methods=['GET'])
-def list_uploaded_files():
+def list_uploaded_documentation():
+
+    type = request.args.get('type', '')
+
     try:
-        files = os.listdir(FEATURE_UPLOAD_FOLDER)
+        if(type == 'feature'):
+            files = os.listdir(FEATURE_UPLOAD_FOLDER)
+        elif(type == 'documentation'):
+            files = os.listdir(DOCUMENTATION_UPLOAD_FOLDER)
         return jsonify({'files': files}), 200
+    
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 
 
 @app.route('/delete_file', methods=['POST'])
 def delete_file():
+
+    type = request.args.get('type', '')
+
     try:
         file_name = request.json.get('file_name')
         if not file_name:
             return jsonify({'error': 'No file name provided'}), 400
 
-        file_path = os.path.join(FEATURE_UPLOAD_FOLDER, file_name)
+        if(type == 'feature'):
+            file_path = os.path.join(FEATURE_UPLOAD_FOLDER, file_name)
+        elif(type == 'documentation'):
+            file_path = os.path.join(DOCUMENTATION_UPLOAD_FOLDER, file_name)
 
         if os.path.exists(file_path):
             os.remove(file_path)
@@ -105,35 +123,50 @@ def delete_file():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/upload_documentation', methods=['POST'])
-def upload_documentation():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+@app.route('/upload_file_for_editing', methods=['POST'])
+def upload_file_for_editing():
 
-    files = request.files.getlist('file')
+    file = request.files.get('file')
 
-    for file in files:
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
+    if file:
+        file_content = file.read().decode('utf-8')
+        return jsonify({'content': file_content}) 
+    
+    return jsonify({'error': 'No file uploaded'}), 400
 
-        if file:
-            file.save(os.path.join(app.config['DOCUMENTATION_UPLOAD_FOLDER'], file.filename))
+@app.route('/save_edited_file', methods=['POST'])
+def save_edited_file():
 
-@app.route('/upload_for_editing', methods=['POST'])
-def upload_for_editing():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+    operation_type = request.args.get('type', '')
 
-    files = request.files.getlist('file')
+    data = request.get_json()
+    content = data.get('content')
+    name = data.get('name')
+    new_name = data.get('new_name')  
 
-    for file in files:
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
+    if not content:
+        return jsonify({'error': 'No content provided'}), 400
 
-        if file:
-            file.save(os.path.join(app.config['EDITOR_UPLOAD_FOLDER'], file.filename))
+    if operation_type == 'save':
+        if not name:
+            return jsonify({'error': 'File name not provided for save'}), 400
 
-    return jsonify({'message': 'Files successfully uploaded'}), 200
+        file_path = os.path.join('editor', name)
+        with open(file_path, 'w') as f:
+            f.write(content)
+        return jsonify({'message': 'File saved successfully'})
+
+    elif operation_type == 'save_as':
+        new_name = data.get('new_name')  
+        if not new_name:
+            return jsonify({'error': 'New file name not provided for save_as'}), 400
+
+        file_path = os.path.join('editor', new_name)
+        with open(file_path, 'w') as f:
+            f.write(content)
+        return jsonify({'message': f'File saved as {new_name} successfully'})
+
+    return jsonify({'error': 'Invalid operation type'}), 400
 
 @app.route('/run_test', methods=['GET'])
 def run_test():
@@ -161,55 +194,58 @@ def download_report_html():
 def download_report_json():
     return send_from_directory(directory=os.getcwd(), path='report.json')
 
-@app.route('/generate_feature', methods=['POST'])
+@app.route('/generate_feature', methods=['GET'])
 def generate_feature():
-    os.system("python generic_api_testing\\generator\\GenerateFeature.py")
+    file = request.args.get('file', '')
+    endpoints = request.args.get('endpoints', '')
+    print(f"python generic_api_testing/generator/useTestCase.py \"{file}\" \"{endpoints}\"")
+    os.system(f"python generic_api_testing/generator/useTestCase.py \"{file}\" \"{endpoints}\"")
     return jsonify({'message': 'Generating Tests!'}), 200
 
 @app.route('/glossary')
 def glossary():
-  return render_template(
+    return render_template(
     'glossary.html',
     title="Glossary - AT4QA Project"
-  )
+    )
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
 
 def generate_output(tags, report_html_url, report_json_url):
-  try:
-      command = ["pytest", "generic_api_testing/tester/step_defs/custom/test_Custom.py"]
-      if tags:
-          command.append(f"-m {tags}")
+    try:
+        command = ["pytest", "generic_api_testing/tester/step_defs/custom/test_Custom.py"]
+        if tags:
+            command.append(f"-m {tags}")
 
-      pytest_process = subprocess.Popen(
-          command,
-          stdout=subprocess.PIPE,
-          stderr=subprocess.STDOUT,
-          universal_newlines=True
-      )
+        pytest_process = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True
+        )
 
-      for line in iter(pytest_process.stdout.readline, ''):
-          yield f"data: {line}\n\n"
+        for line in iter(pytest_process.stdout.readline, ''):
+            yield f"data: {line}\n\n"
 
-      pytest_process.stdout.close()
-      pytest_process.wait()
+        pytest_process.stdout.close()
+        pytest_process.wait()
 
-      if pytest_process.returncode == 0:
-          yield "data: Tests completed successfully!\n\n"
-      else:
-          yield "data: Tests failed!\n\n"
+        if pytest_process.returncode == 0:
+            yield "data: Tests completed successfully!\n\n"
+        else:
+            yield "data: Tests failed!\n\n"
 
-      report_html_path = os.path.join('static', 'reports', 'report.html')
-      report_json_path = os.path.join('static', 'reports', '.report.json')
+        report_html_path = os.path.join('static', 'reports', 'report.html')
+        report_json_path = os.path.join('static', 'reports', '.report.json')
 
-      if os.path.exists(report_html_path) and os.path.exists(report_json_path):
-          yield f"data: Report available: {report_html_url}\n\n"
-          yield f"data: Report available: {report_json_url}\n\n"
-      else:
-          yield "data: No report files found.\n\n"
+        if os.path.exists(report_html_path) and os.path.exists(report_json_path):
+            yield f"data: Report available: {report_html_url}\n\n"
+            yield f"data: Report available: {report_json_url}\n\n"
+        else:
+            yield "data: No report files found.\n\n"
 
-      yield "event: end\ndata: end\n\n"
+        yield "event: end\ndata: end\n\n"
 
-  except Exception as e:
-      yield f"data: Error: {str(e)}\n\n"
+    except Exception as e:
+        yield f"data: Error: {str(e)}\n\n"
